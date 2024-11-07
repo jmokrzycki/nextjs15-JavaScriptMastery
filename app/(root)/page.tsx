5f011ed53a00447a43af2f5a47e9c8acdc45286d
jmokrzycki <jarek.mokrzycki@gmail.com>
2024-11-08 00:24:03 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Enrepreneurs</h1>
+          <p className="sub-headeing !max-w-3xl">
+            Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          </p>
+      </section>
+    </div>
+  );
+}
